739b9e28a8db1431103e199fa59b246e0aab8829
AdityaRaj00007 <rajgupta49102@gmail.com>
2025-01-08 10:42:02 +0530

    Practice

---
+++ Practice Problem/AddTwo.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class AddTwo
+{
+ public static void main(String args[])
+ {
+  Scanner sc = new Scanner(System.in);
+  int a = sc.nextInt();
+  int b = sc.nextInt();
+  int res = a+b;
+  System.out.print(res);
+ }
+}
---
+++ Practice Problem/xy.java
@@ -0,0 +1,7 @@
+class xy
+{
+ public static void main(String args [])
+ {
+  System.out.println("Welcome to Bridgelabz!");
+ }
+}
